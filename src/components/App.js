ee795fa171d3523dfdb795fcbf814b325d8fe6a1
alchemist1111 <emoyoeugine@gmail.com>
2024-05-03 00:41:23 +0300

    adds content in component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React, { useState, useEffect} from 'react';
+function App() {
+const [isDogPic, setIsDogPic] = useState(null);
+
+useEffect(() => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then((response) => response.json())
+  .then((data) => {
+    setIsDogPic(data.message)});
+}, []);
+
+if (!isDogPic) return <p>Loading...</p>
+
+return <img src={isDogPic} alt="A Random Dog" />
+}
+
+export default App;
